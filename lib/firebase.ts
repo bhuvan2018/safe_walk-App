ddebc4c4bfaace9f6367140357647bff70b5ff21
Bhuvan_shetty <bhuvanshetty2018@gmail.com>
2025-03-31 16:24:02 +0530

    commited

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
-
 const firebaseConfig = {
   apiKey: "AIzaSyB7gG1qr-AdyqaDqkDp_Tw1dVj6-NHy9e8",
   authDomain: "myauthapp-9c735.firebaseapp.com",
@@ -9,9 +8,6 @@ const firebaseConfig = {
   messagingSenderId: "526238519996",
   appId: "1:526238519996:web:7ca1c25500e64f6cf176b9",
 }
-
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
-
 export { auth }
-
